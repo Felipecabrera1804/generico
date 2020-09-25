3c7574b773eac25f288683df40a2538eee5f5f0d
Felipe <felipe.cabrera@correo.ucu.edu.uy>
2020-09-25 20:21:41 -0300

    genericos

--- src/Library/IObservable.cs
+++ src/Library/IObservable.cs
@@ -1,9 +1,12 @@
+using System;
+
 namespace Observer
 {
-    public interface IObservable
+    public interface IObservable<T>
     {
-        void Subscribe(IObserver observer);
+        void Subscribe(IObserver<T> observer);
 
-        void Unsubscribe(IObserver observer);
+        void Unsubscribe(IObserver<T> observer);
     }
+
 }
